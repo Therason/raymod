3f02ec9914b0e5bed776469acc6587ed4113d780
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-04-29 23:17:01 -0400

    Change to temp favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
@@ -9,13 +8,13 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="RAYMOD" />
+        <title>RAYMOD</title>
+        <meta name="description" content="ray portfolio stuffs" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-
+        <h1>RAYMOD!!!</h1>
       </main>
     </>
   )
