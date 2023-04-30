27e68450c120cdac2c9d448539d7fd4ec6d27870
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-04-29 23:53:51 -0400

    Add basic file upload

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,10 +1,29 @@
 import styles from '@/styles/Upload.module.css'
+import { useState } from 'react'
 
 export default function Upload() {
+  const [ image, setImage ] = useState<File>()
+
+  const selectImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFiles = e.target.files as FileList
+    setImage(selectedFiles?.[0])
+  }
+
+  const handleSubmit = () => {
+    console.log('heyyy :)')
+    console.log(image)
+  }
+
   return (
     <>
       <main className={`${styles.main}`}>
-        <h1>RAYMOD!!!</h1>
+        <h1>Upload :D</h1>
+        <div>
+          <label>
+            <input type="file" accept="image/*" onChange={selectImage} />
+          </label>
+          <button onClick={handleSubmit}>submit!!</button>
+        </div>
       </main>
     </>
   )
