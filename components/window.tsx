1a035cbb68ef575f1edae7b29420a54add2ff8ac
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-05-07 00:26:19 -0400

    Finish transition to window component

--- components/window.tsx
+++ components/window.tsx
@@ -3,8 +3,10 @@ import styles from '@/styles/Window.module.css'
 export default function Window({src, alt, size}: {src: string, alt: string, size: string}) {
   return (
     <div className={`border ${styles.container}`} style={{ minWidth: size, minHeight: size}}>
-      <span className={styles.bar}><p>x</p></span>
-      <img src={src} alt={alt} />
+      <span className={styles.bar}>
+        <img className={styles.icon} src='/close-icon.png'></img>
+      </span>
+      <img className={styles.image} src={src} alt={alt} />
     </div>
   )
 }
