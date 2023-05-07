5a3e64bdd0eaa7cabefacbbbdf07e80fe148266c
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-05-07 16:20:07 -0400

    Basic upload success notification

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -3,10 +3,12 @@ import { useState } from 'react'
 
 export default function Upload() {
   const [ image, setImage ] = useState<File>()
+  const [ uploaded, setUploaded ] = useState(false)
 
   const selectImage = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFiles = e.target.files as FileList
     setImage(selectedFiles?.[0])
+    setUploaded(false)
   }
 
   const handleSubmit = async () => {
@@ -19,6 +21,7 @@ export default function Upload() {
     })
     const data = await res.json()
     console.log(data)
+    setUploaded(true)
   }
 
   return (
@@ -30,6 +33,7 @@ export default function Upload() {
             <input type="file" accept="image/*" onChange={selectImage} />
           </label>
           <button onClick={handleSubmit}>submit!!</button>
+          {uploaded && <p style={{ color: '#019563' }}>File uploaded successfully!</p>}
         </div>
         <div className="gallery"></div>
       </main>
