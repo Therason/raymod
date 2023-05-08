b98bd75886e8fb65743673f268d09f51b5add653
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-05-08 12:12:08 -0400

    Flesh out grid styling for BigWindow

--- components/bigWindow.tsx
+++ components/bigWindow.tsx
@@ -3,7 +3,15 @@ import styles from '@/styles/BigWindow.module.css'
 export default function BigWindow({image}: {image: any}) {
   return (
     <div className={`border ${styles.container}`}>
-      <img className={styles.image} src={image.url} alt={image.alt || ''} />
+      <span className={styles.bar}>
+        <img className={`border ${styles.icon}`} src='/close-icon.png'></img>
+      </span>
+      <div className={styles.content}>
+        <img className={styles.image} src={image.url} alt={image.alt || ''} />
+        <div className={styles.info}>
+          <p>{image.description}</p>
+        </div>
+      </div>
     </div>
   )
 }
