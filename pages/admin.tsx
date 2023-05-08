80144119dc498e419c2c6ed9b54552575cbe254d
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-05-08 08:35:16 -0400

    Begin working on admin gallery panel

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -2,10 +2,14 @@ import styles from '@/styles/Upload.module.css'
 import { useState } from 'react'
 
 export default function Upload() {
+  // upload form state
   const [ image, setImage ] = useState<File>()
   const [ description, setDescription ] = useState('')
   const [ alt, setAlt ] = useState('')
   const [ uploaded, setUploaded ] = useState(false)
+
+  // navigation state
+  const [ gallery, setGallery ] = useState(false)
 
   const selectImage = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFiles = e.target.files as FileList
@@ -31,21 +35,30 @@ export default function Upload() {
     <>
       <main className={`${styles.main}`}>
         <h1>Admin Stuff :D</h1>
-        <div>
-          <label>
-            <input type="file" accept="image/*" onChange={selectImage} />
-          </label>
-          <button onClick={handleSubmit}>submit!!</button>
-          {uploaded && <p style={{ color: '#019563' }}>File uploaded successfully!</p>}
-        </div>
-        <div>
-          <input type='text' placeholder='image description' onChange={(e) => setDescription(e.target.value)} />
-        </div>
-        <div>
-          <input type='text' placeholder='image alt text' onChange={(e) => setAlt(e.target.value)} />
-        </div>
-        <div className="gallery"></div>
+        {/* UI switcher */}
+        <button onClick={() => setGallery(!gallery)}>{gallery ? 'upload...' : 'gallery...'}</button>
+
+        {/* file upload */}
+        {!gallery &&      
+          <div className={styles.upload}>
+            <label>
+              <input type="file" accept="image/*" onChange={selectImage} />
+            </label>
+            <button onClick={handleSubmit}>submit!!</button>
+            {uploaded && <p style={{ color: '#019563' }}>File uploaded successfully!</p>}
+            <input type='text' placeholder='image description' onChange={(e) => setDescription(e.target.value)} />
+            <input type='text' placeholder='image alt text' onChange={(e) => setAlt(e.target.value)} />
+          </div>
+        }
+
+        {/* gallery config */}
+        {gallery && 
+          <div className={styles.gallery}>
+
+          </div>
+        }
       </main>
     </>
   )
 }
+
---
+++ pages/api/images.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import connect from '@/lib/db'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    res.status(422).json({ message: "Route not valid" });
+    return;
+  }
+
+  const conn = await connect()
+  const db = conn.db()
+  const images = (await db.collection('posts').find({}).sort({_id: -1}).toArray()).map((document) => {
+    return {
+      ...document,
+      _id: document._id.toString()
+    }
+  })
+  conn.close()
+
+  res.status(200).json({ images })
+}
