1ce5b9150fba541c3a01ec2dc9f3603c885f731f
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-05-08 09:00:19 -0400

    Use Window component within admin gallery

--- components/adminGallery.tsx
+++ components/adminGallery.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import styles from '@/styles/AdminGallery.module.css'
 import Image from 'next/image'
+import Window from './window'
 
 export default function AdminGallery() {
   const [ images, setImages ] = useState([])
@@ -13,13 +14,16 @@ export default function AdminGallery() {
 
   return (
     <>
-      <h1>Admin Gallery</h1>
+      <h2>Admin Gallery</h2>
       <div className={styles.container}>
         {images.map((image: any) => {
           return (
-            <div key={image._id} className={styles.image}>
-              <Image src={image.url} alt={image.alt} fill />
-            </div>
+            // <div key={image._id} className={styles.image}>
+            //   <Image src={image.url} alt={image.alt} fill />
+            // </div>
+            <Window key={image._id} src={image.url} alt={image.alt} size="250px" handleClick={() => {
+              console.log('ID:', image._id)
+            }}/>
           )
         })}
       </div>
--- components/window.tsx
+++ components/window.tsx
@@ -1,10 +1,10 @@
 import styles from '@/styles/Window.module.css'
 
-export default function Window({src, alt, size}: {src: string, alt: string, size: string}) {
+export default function Window({src, alt, size, handleClick}: {src: string, alt: string, size: string, handleClick: any}) {
   return (
-    <div className={`border ${styles.container}`} style={{ minWidth: size, minHeight: size}}>
+    <div className={`border ${styles.container}`} style={{ width: size, height: size, minWidth: size, minHeight: size}}>
       <span className={styles.bar}>
-        <img className={`border ${styles.icon}`} src='/close-icon.png'></img>
+        <img className={`border ${styles.icon}`} src='/close-icon.png' onClick={handleClick}></img>
       </span>
       <img className={styles.image} src={src} alt={alt} />
     </div>
