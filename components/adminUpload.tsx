629c94a890bc988c7e3e95a0da2074f7aa077536
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-05-16 05:19:53 -0400

    Edit admin upload form layout

--- components/adminUpload.tsx
+++ components/adminUpload.tsx
@@ -1,5 +1,6 @@
 import styles from '@/styles/AdminUpload.module.css'
 import { useState } from 'react'
+import Image from 'next/image'
 
 export default function AdminUpload(
   { revalidate, image, setImage, uploaded, setUploaded }: 
@@ -7,6 +8,7 @@ export default function AdminUpload(
 ) {
   const [ description, setDescription ] = useState('')
   const [ alt, setAlt ] = useState('')
+  const [ imageUrl, setImageUrl ] = useState('')
 
   const handleSubmit = async () => {
     const form = new FormData()
@@ -32,19 +34,24 @@ export default function AdminUpload(
 
   return (
     <div className={styles.upload}>
-      <div>
-        <label>
-          <input type="file" accept="image/*" onChange={(e) => {
-            const selectedFiles = e.target.files as FileList
-            setImage(selectedFiles?.[0])
-            setUploaded(false)
-          }} />
-        </label>
+      <div className={styles.form}>
+        <h3>select image</h3>
+        <input type="file" accept="image/*" onChange={(e) => {
+          const selectedFiles = e.target.files as FileList
+          setImage(selectedFiles?.[0])
+          setImageUrl(URL.createObjectURL(selectedFiles?.[0]))
+          setUploaded(false)
+        }} />
+        <textarea value={description} placeholder='image description' onChange={(e) => setDescription(e.target.value)} />
+        <input className={styles.alt} value={alt} type='text' placeholder='image alt text' onChange={(e) => setAlt(e.target.value)} />
         <button onClick={handleSubmit}>submit!!</button>
         {uploaded && <p style={{ color: '#019563' }}>File uploaded successfully!</p>}
-        <input value={alt} type='text' placeholder='image alt text' onChange={(e) => setAlt(e.target.value)} />
       </div>
-      <textarea value={description} rows={3} cols={40} placeholder='image description' onChange={(e) => setDescription(e.target.value)} />
+      {imageUrl && 
+        <div style={{ position: 'relative', width: '100%', height: '300px' }}>
+          <Image src={imageUrl} alt='selected image' fill style={{ objectFit: 'contain' }} />
+        </div>
+      }
     </div>
   )
 }
