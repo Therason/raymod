d948e3bc6bb65a0412569d314b24e36160d8bd18
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-05-17 07:50:03 -0400

    Convert all img to Image tags, fix styling bugs

--- components/adminWindow.tsx
+++ components/adminWindow.tsx
@@ -1,6 +1,8 @@
 import styles from '@/styles/AdminWindow.module.css'
 import { useState } from 'react'
 import { Draggable } from 'react-beautiful-dnd'
+import CloseIcon from './closeIcon'
+import Image from 'next/image'
 
 export default function AdminWindow(
   {src, alt, size, handleClick, id, index, description, setImages}: {src: string, alt: string, size: string, handleClick: any, id: string, index: number, description: string, setImages: any}
@@ -13,10 +15,12 @@ export default function AdminWindow(
         <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} style={{display: 'flex', justifyContent: 'center', ...provided.draggableProps.style}}>
           <div className={`border ${styles.container}`}>
             <span className={styles.bar}>
-              <img className={`border ${styles.icon}`} src='/close-icon.png' onClick={handleClick}></img>
+              <CloseIcon handleClick={handleClick} background='#eddcd1' />
             </span>
             <div className={styles.content}>
-              <img className={styles.image} src={src} alt={alt} />
+              <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+                <Image src={src} alt={alt || ''} fill style={{ objectFit: 'contain' }} />
+              </div>
               <textarea value={desc} className={styles.info} onChange={(e) => {
                 setDesc(e.target.value)
                 // this works i guess
--- components/bigWindow.tsx
+++ components/bigWindow.tsx
@@ -1,13 +1,17 @@
 import styles from '@/styles/BigWindow.module.css'
+import Image from 'next/image'
+import CloseIcon from './closeIcon'
 
 export default function BigWindow({image}: {image: any}) {
   return (
     <div className={`border ${styles.container}`}>
       <span className={styles.bar}>
-        <img className={`border ${styles.icon}`} src='/close-icon.png'></img>
+        <CloseIcon handleClick={() => {}} background='#eddcd1' />
       </span>
       <div className={styles.content}>
-        <img className={styles.image} src={image.url} alt={image.alt || ''} />
+        <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+          <Image src={image.url} alt={image.alt || ''} className={styles.image} fill style={{ objectFit: 'contain' }} />
+        </div>
         <div className={styles.info}>
           <p>{image.description}</p>
         </div>
