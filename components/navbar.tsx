75edad5c8883325c08843b3959d861f9c78c72ca
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-05-26 19:31:55 -0400

    Edit navbar state

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,34 @@
 import Link from 'next/link'
 import styles from '@/styles/Navbar.module.css'
 import { useSession } from 'next-auth/react'
+import { useState, useEffect } from 'react'
 
 export default function Navbar() {
-  const {data: session, status} = useSession()
+  const [ tabs, setTabs ] = useState([
+    {name: 'home', href: '/'},
+    {name: 'gallery', href: '/gallery'},
+    {name: 'contact', href: '/contact'},
+  ])
+  const {data: session} = useSession()
+
+  useEffect(() => {
+    if (session) {
+      setTabs([
+        ...tabs,
+        {name: 'admin', href: '/admin'}
+      ])
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [session])
+
   return (
     <nav className={styles.container}>
-      <Link href="/">home</Link>
-      <Link href="/gallery">gallery</Link>
+      {tabs.map((tab) => 
+        <Link key={tab.name} href={tab.href}>
+          <span>{tab.name}</span>
+        </Link>
+      )}
       {!session && <Link href="/hush/login">login</Link>}
-      {session && <Link href="/admin">admin</Link>}
     </nav>
   )
 }
