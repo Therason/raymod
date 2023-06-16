a30f21c6b4b403981e657ad271f546359dfc9ec1
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-06-15 20:22:09 -0400

    Add Lenis for smooth scrolling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { SessionProvider } from 'next-auth/react'
 import { AnimatePresence } from 'framer-motion'
 import { useRouter, Router } from 'next/router'
 import { useEffect } from 'react'
+import Lenis from '@studio-freight/lenis'
 
 const benderBold = localFont({ src: '../public/Bender_Bold.otf' })
 const bender = localFont({ src: '../public/Bender.otf' })
@@ -15,7 +16,7 @@ const bender = localFont({ src: '../public/Bender.otf' })
 const routeChange = () => {
   const fix = () => {
     const allStyleElems = document.querySelectorAll('style[media="x"]')
-    allStyleElems.forEach(el => {
+    allStyleElems.forEach((el) => {
       el.removeAttribute('media')
     })
   }
@@ -24,13 +25,27 @@ const routeChange = () => {
 Router.events.on('routeChangeComplete', routeChange)
 Router.events.on('routeChangeStart', routeChange)
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   // page components need unique keys for AnimatePresence to work
   const router = useRouter()
   const pageKey = router.asPath
 
   useEffect(() => {
     router.push(router.pathname)
+    const lenis = new Lenis()
+    lenis.on('scroll', (e: any) => {
+      console.log(e)
+    })
+
+    function raf(time: any) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
@@ -38,14 +53,20 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
     <SessionProvider session={session}>
       <Head>
         <title>RAYMOD</title>
-        <meta name="description" content="ray portfolio stuffs" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='ray portfolio stuffs' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <div className={benderBold.className}>
-        <span className={bender.className}><Navbar /></span>
+        <span className={bender.className}>
+          <Navbar />
+        </span>
         <AnimatePresence mode='wait'>
-          <Component key={pageKey} className={benderBold.className} {...pageProps} />
+          <Component
+            key={pageKey}
+            className={benderBold.className}
+            {...pageProps}
+          />
         </AnimatePresence>
       </div>
     </SessionProvider>
